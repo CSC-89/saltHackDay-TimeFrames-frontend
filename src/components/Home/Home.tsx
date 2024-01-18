6cb18a510791b021ce4f3c3cdba146dafb5e8118
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 11:56:02 +0100

    add: current date as default value

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,15 +5,17 @@ import DoughnutContainer from "../DoughnutContainer/DoughnutContainer";
 import Navbar from "../Navbar/Navbar";
 import { DatePicker } from "@mui/x-date-pickers";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 const Home = () => {
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState(dayjs(new Date));
+
   return (
     <div className="flex flex-col items-center">
       <Navbar />
       <main className="p-5">
         <div className="mx-auto flex justify-center my-3">
-          <DatePicker label="SelectDate" value={date} onChange={newDate => setDate(newDate)} />
+          <DatePicker label="SelectDate" value={date} onChange={newDate => setDate(dayjs(newDate))} />
         </div>
         <TimeForm />
         <TaskForm />
