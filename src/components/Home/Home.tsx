f594810b3c6b57578a4866e83e0e23b5f30d3066
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 10:22:52 +0100

    add: styling

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,18 +1,21 @@
-import TimeForm from '../TimeForm/TimeForm'
-import TaskForm from '../TaskForm/TaskForm'
-import TaskContainer from '../TaskContainer/TaskContainer'
-import DoughnutContainer from '../DoughnutContainer/DoughnutContainer'
+import TimeForm from "../TimeForm/TimeForm";
+import TaskForm from "../TaskForm/TaskForm";
+import TaskContainer from "../TaskContainer/TaskContainer";
+import DoughnutContainer from "../DoughnutContainer/DoughnutContainer";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div>Home
-      <TimeForm />
-      <TaskForm />
-      <TaskContainer />
-      <DoughnutContainer />
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <main>
+        <TimeForm />
+        <TaskForm />
+        <TaskContainer />
+        <DoughnutContainer />
+      </main>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
