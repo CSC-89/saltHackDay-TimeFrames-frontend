3d22368b6f6abb2ae8b3a1fbcf23678652019250
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-18 09:53:43 +0100

    add: navbar component

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,6 +1,11 @@
+import logo from "../../assets/TF-logo.png";
+
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+      <img src={logo} alt="Timeframes logo" />
+      <h1>Don't be blind to your free time</h1>
+    </div>
   )
 }
 
