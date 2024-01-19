e9ca5b65727b7e39dc8d147ad84878162fca9110
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-19 09:51:31 +0100

    add: buttons

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,5 +1,5 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import { FC, useContext } from "react";
+import { FC, SyntheticEvent, useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/userContext";
 import { Task } from "../../types/GlobalTypes";
 
@@ -15,17 +15,22 @@ type TaskFormProps = {
 };
 
 const TaskForm: FC<TaskFormProps> = ({ addTask }) => {
+  const [type, setType] = useState<HTMLButtonElement>();
   const { register, handleSubmit } = useForm<FormValues>();
   const ctx = useContext(UserContext);
 
   const addTaskHandler: SubmitHandler<FormValues> = (data) => {
+    if(!type) throw new Error("Must declare a type");
+
     const contentInput = document.getElementById(
       "content-input"
     ) as HTMLInputElement;
     const completionInput = document.getElementById(
       "completion-input"
     ) as HTMLInputElement;
-    const typeValues = data.typeInfo.split("-");
+    
+    const typeValues = type!.value.split("-");
+    console.log(typeValues[1])
 
     addTask({
       content: data.content,
@@ -39,39 +44,93 @@ const TaskForm: FC<TaskFormProps> = ({ addTask }) => {
     completionInput.value = "";
   };
 
+  const setTypeHandler = (evt: SyntheticEvent) => {
+    const target = evt.target as HTMLButtonElement;
+    if (type) {
+      type.classList.replace("border-white", "border-transparent");
+      target.classList.replace("border-transparent", "border-white");
+    }
+
+    setType(target);
+  };
+
+  useEffect(() => {
+    const starterTypeButton = document.getElementById("starter-type") as HTMLButtonElement;
+    setType(starterTypeButton)
+  }, [])
+  console.log(type);
   return (
     <section className="border shadow-md rounded-lg bg-primary opacity-90 w-full mx-auto px-6 py-2 my-3">
       <form className="flex flex-col" onSubmit={handleSubmit(addTaskHandler)}>
         <input
           id="content-input"
-          className="my-2 rounded-sm pl-2"
+          className="my-2 rounded-md border border-gray-600 pl-2"
           placeholder="What needs doing today?"
           {...register("content", { required: "This is required" })}
         />
         <div className="flex justify-left my-2">
-          <label htmlFor="completionTime">
-            Duration:
-          </label>
+          <label htmlFor="completionTime">Duration:</label>
           <input
             id="completion-input"
-            className="w-12 rounded-sm mx-4 text-center"
+            className="w-12 rounded-md border border-gray-600 mx-4 text-center"
             type="number"
             {...register("completionTime", { required: "This is required" })}
           />
           Minutes
         </div>
         <div className="my-2">
-          Type:
-          <select
-            id="type-input"
-            className="rounded-sm ml-1"
-            {...register("typeInfo", { required: "This is required" })}
-          >
-            <option value="Exercise-purple">Exercise</option>
-            <option value="Cleaning-blue">Cleaning</option>
-            <option value="Study-yellow">Study</option>
-            <option value="Other-teal">Other</option>
-          </select>
+        <h2 className="w-full mb-2 text-center bg-blue-100 rounded-md">Type</h2>
+          <div className="grid grid-cols-3 items-center">
+            <button
+              id="starter-type"
+              onClick={setTypeHandler}
+              type="button"
+              value="Exercise-purple"
+              className="m-2 bg-purple-500 text-secondary w-20 rounded-md shadow-md mx-auto border border-white "
+            >
+              Exercise
+            </button>
+            <button
+              onClick={setTypeHandler}
+              type="button"
+              value="Cleaning-blue"
+              className="m-2 bg-blue-500 text-secondary w-20 rounded-md shadow-md mx-auto border border-transparent"
+            >
+              Cleaning
+            </button>
+            <button
+              onClick={setTypeHandler}
+              type="button"
+              value="Study-yellow"
+              className="m-2 bg-yellow-500 text-black w-20 rounded-md shadow-md mx-auto border border-transparent"
+            >
+              Study
+            </button>
+            <button
+              type="button"
+              onClick={setTypeHandler}
+              value="Shopping-brown"
+              className="m-2 bg-brown-500 text-secondary w-20 rounded-md shadow-md mx-auto border border-transparent"
+            >
+              Shopping
+            </button>
+            <button
+              type="button"
+              onClick={setTypeHandler}
+              value="Travel-red"
+              className="m-2 bg-red-500 text-secondary w-20 rounded-md shadow-md mx-auto border border-transparent"
+            >
+              Travel
+            </button>
+            <button
+              type="button"
+              onClick={setTypeHandler}
+              value="Other-teal"
+              className="m-2 bg-teal-500 text-secondary w-20 rounded-md shadow-md mx-auto border border-transparent"
+            >
+              Other
+            </button>
+          </div>
         </div>
         <button
           type="submit"
--- src/components/TimeForm/TimeForm.tsx
+++ src/components/TimeForm/TimeForm.tsx
@@ -11,81 +11,96 @@ type FormValues = {
 };
 
 type TimeFormProp = {
-  freeTime: number,
-  date: string,
-  updateFreeTime: (hours: BusyHours) => void
-}
+  freeTime: number;
+  date: string;
+  updateFreeTime: (hours: BusyHours) => void;
+};
 
-const TimeForm: FC<TimeFormProp> = ({date, updateFreeTime, freeTime}) => {
+const TimeForm: FC<TimeFormProp> = ({ date, updateFreeTime, freeTime }) => {
   const [selected, setSelected] = useState(false);
   const { register, handleSubmit } = useForm<FormValues>();
 
   const submitHandler: SubmitHandler<FormValues> = (data: FormValues) => {
-    updateFreeTime({workTime: data.workTime, wakeTime: data.wakeTime, sleepTime: data.sleepTime});
+    updateFreeTime({
+      workTime: data.workTime,
+      wakeTime: data.wakeTime,
+      sleepTime: data.sleepTime,
+    });
     setSelected(true);
   };
 
   const resetHoursHandler = () => {
-    updateFreeTime({workTime: 0, wakeTime: 0, sleepTime: 0 });
+    updateFreeTime({ workTime: 0, wakeTime: 0, sleepTime: 0 });
     setSelected(false);
-  }
+  };
 
   return (
     <section className="shadow-md rounded-lg bg-primary opacity-90 w-full mx-auto px-6 py-2">
-      {!selected ? <form className="flex flex-col" onSubmit={handleSubmit(submitHandler)}>
-        <input {...register("date", { required: "This is required" })} value={date} hidden/>
-        <div className="my-2 flex justify-between">
-          <label htmlFor="workInput" className="flex justify-between">
-            Working Hours:
-          </label>
+      {!selected ? (
+        <form className="flex flex-col" onSubmit={handleSubmit(submitHandler)}>
+          <input
+            {...register("date", { required: "This is required" })}
+            value={date}
+            hidden
+          />
+          <div className="my-2 flex justify-between">
+            <label htmlFor="workInput" className="flex justify-between">
+              Working Hours:
+            </label>
             <input
-            id="work-input"
-              className="w-12 rounded-sm ml-4 text-center"
+              id="work-input"
+              className="w-12 rounded-md ml-4 text-center border border-gray-600"
               type="number"
               min={0}
               max={12}
               {...register("workTime", { required: "This is required" })}
             />
-        </div>
-        <h2 className="w-full mb-2 text-center bg-secondary rounded-md">Day length (hours)</h2>
-        <div>
-          <label htmlFor="waking-hour">
-            Wake Up
-            <input
-            id="wake-input"
-              className="w-12 rounded-sm ml-4 text-center"
-              type="number"
-              min={0}
-              {...register("wakeTime", { required: "This is required" })}
-            />
-          </label>
-          <label className="pl-6">
-            Bed
-            <input
-            id="sleep-input"
-              className="w-12 rounded-sm ml-4 text-center"
-              type="number"
-              min={0}
-              max={23}
-              {...register("sleepTime")}
-            />
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="m-2 mt-4 bg-buttonSubmit text-buttonSubmitFont w-28 rounded-md shadow-md mx-auto "
-        >
-          Set Free Time
-        </button>
-      </form> : (
+          </div>
+          <h2 className="w-full mb-2 text-center bg-blue-100 rounded-md">
+            Day length (hours)
+          </h2>
+          <div>
+            <label htmlFor="waking-hour">
+              Wake Up
+              <input
+                id="wake-input"
+                className="w-12 rounded-md border border-gray-600 ml-4 text-center"
+                type="number"
+                min={0}
+                {...register("wakeTime", { required: "This is required" })}
+              />
+            </label>
+            <label className="pl-6">
+              Bed
+              <input
+                id="sleep-input"
+                className="w-12 rounded-md border border-gray-600 ml-4 text-center"
+                type="number"
+                min={0}
+                max={23}
+                {...register("sleepTime")}
+              />
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="m-2 mt-4 bg-buttonSubmit text-buttonSubmitFont border border-gray-600 w-28 rounded-md shadow-md mx-auto "
+          >
+            Set Free Time
+          </button>
+        </form>
+      ) : (
         <div className="flex flex-col justify-center h-28">
-        <h2 className="text-md text-center">You have <span className="text-secondary">{freeTime} hours</span> of free time today!</h2>
-        <button
-          onClick={resetHoursHandler}
-          className="m-2 bg-buttonSubmit text-buttonSubmitFont w-28 rounded-md shadow-md mx-auto "
-        >
-          Reset
-        </button>
+          <h2 className="text-md text-center">
+            You have <span className="text-secondary">{freeTime} hours</span> of
+            free time today!
+          </h2>
+          <button
+            onClick={resetHoursHandler}
+            className="m-2 bg-buttonSubmit text-buttonSubmitFont w-28 rounded-md shadow-md mx-auto "
+          >
+            Reset
+          </button>
         </div>
       )}
     </section>
