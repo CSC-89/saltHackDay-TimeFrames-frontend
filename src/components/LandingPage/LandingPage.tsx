e58ca79ea419c42410a942af91adb0fa1501a793
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-19 08:58:43 +0100

    add: styling - landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -2,9 +2,9 @@ import logo from "../../assets/TF-logo.png";
 
 const LandingPage = () => {
   return (
-    <div className="bg-primary opacity-80 min-h-screen flex flex-col">
-      <img src={logo} alt="Timeframes logo" />
-      <h1 className="mx-auto">Don't be blind to your free time</h1>
+    <div className="bg-primary opacity-80 min-h-screen flex flex-col items-center">
+      <img src={logo} alt="Timeframes logo" className=" max-w-96"/>
+      <h1 className="mx-auto lg:text-2xl">Don't be blind to your free time</h1>
     </div>
   )
 }
